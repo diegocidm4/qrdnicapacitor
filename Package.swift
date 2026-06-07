ef9f742de46c7ac6bd28e98e12af51918fb4423b
diegocidm4 <diegocidm4@hotmail.com>
2026-06-07 22:52:19 +0200

    1.0.10

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(url: "https://github.com/diegocidm4/iQRDNI.git", from: "1.0.4"),
+        .package(url: "https://github.com/diegocidm4/iQRDNI.git", from: "1.0.5"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", exact: "1.6.0")
     ],
     targets: [
